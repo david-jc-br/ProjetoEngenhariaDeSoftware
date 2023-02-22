bd373b09d1c4f0f738195281555394a7e18bee1e
David de Jesus Costa <73839667+david-jc-br@users.noreply.github.com>
2023-02-21 23:25:39 -0300

    Delete requistos/Banco

--- requisitos/Banco/inserir.sql
+++
@@ -1,9 +0,0 @@
-use temdevoope;
-INSERT INTO Usuario VALUES(null,'Juara','Rua: Goiano 23','049.276.896-99','34 889704520','clienteajuara','Usuario locador','juara@terra.com.br',1/2/1985,'M');
-INSERT INTO Usuario VALUES(null,'Joao','Rua: Fransisco Sales','046.348.346-03','35 697704920','atendentejoao','Atendente','fundador@solahic.com.br',1/2/1980,'M');
-INSERT INTO Administador VALUES(null,"fundador@solahic.com.br","Cadastramento de Patrimonio",500,1);
-INSERT INTO `Alugado` VALUES(null, 1, 150, 10/02/2023, 29/03/2023);
-INSERT INTO `Alugado` VALUES(null, 2, 350, 2023/02/12, 2023/02/18);
-INSERT INTO Comercial VALUES(null,'umcliente@localiza.com.br','Cliente' ,'104.37498089-94','cliente locador',1);
-
-INSERT INTO Alugado VALUES(null, 1, 240, 2023,02.10, 2023/2/20);
