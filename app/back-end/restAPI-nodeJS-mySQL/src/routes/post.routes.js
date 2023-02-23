f0c09e0b5f6db98096dd06e2f45de3b26187a39c
David de Jesus Costa <73839667+david-jc-br@users.noreply.github.com>
2023-02-23 18:11:36 -0300

    new page html

--- app/back-end/restAPI-nodeJS-mySQL/src/routes/post.routes.js
+++ app/back-end/restAPI-nodeJS-mySQL/src/routes/post.routes.js
@@ -4,11 +4,29 @@ const connection = require('../connection/mysql-connection');
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json(); 
 
+// Rota para inserir a Aluguel 
+postRoutes.post('/aluguel/add', jsonParser, (req, res) => 
+{
+    const {codLocacao, dataDeInicio, dataDeFim, Veiculo_idVeiculo, Cliente_idCliente, Funcionario_idFuncionario, Funcionario_Pessoa_idPessoa} = req.body;
+
+    connection.query("INSERT INTO Locacao (codLocacao, dataDeInicio, dataDeFim, Veiculo_idVeiculo, Cliente_idCliente, Funcionario_idFuncionario, Funcionario_Pessoa_idPessoa) VALUES (?, ?, ?, ?, ?, ?, ?);", [codLocacao, dataDeInicio, dataDeFim, Veiculo_idVeiculo, Cliente_idCliente, Funcionario_idFuncionario, Funcionario_Pessoa_idPessoa],(err, results) => {
+            if (err) {
+                return res.send(err);
+            } else {
+                return res.json({
+                    data: results
+                });
+            }
+        }
+    );
+})
+
+// Rota para inserir a pessoa 
 postRoutes.post('/pessoa/add', jsonParser, (req, res) => 
 {
-    const {NomeCompleto, Cpf, Endereco, Telefone, Email, Sexo,DataNascimento} = req.body;
+    const {NomeCompleto, CPF, Endereco, Telefone, Email, Sexo,DataNascimento} = req.body;
 
-    connection.query("insert into `Pessoa` (NomeCompleto, Cpf, Endereco, Telefone, Email, Sexo, DataNascimento) values(?, ?, ?, ?, ?, ?, ?)", [NomeCompleto,Cpf,Endereco,Telefone,Email,Sexo,DataNascimento],(err, results) => {
+    connection.query("insert into `Pessoa` (NomeCompleto, CPF, Endereco, Telefone, Email, Sexo, DataNascimento ) values(?, ?, ?, ?, ?, ?, ?)", [NomeCompleto,CPF,Endereco,Telefone,Email,Sexo,DataNascimento],(err, results) => {
             if (err) {
                 return res.send(err);
             } else {
